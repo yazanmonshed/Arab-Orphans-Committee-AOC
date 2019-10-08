5daf6c1460fcd3b5c9da830abbb00f8bb50a6ca5
Yazan Al Monshed <yazanalmonshed@localhost.localdomain>
2019-10-09 01:10:40 +0300

    modified navbar js

--- frontend_site/gui/src/components/Navbars.js
+++ frontend_site/gui/src/components/Navbars.js
@@ -1,7 +1,7 @@
  
 import React from 'react'
 
-import {Nav, Navbar, Form, FormControl,Button,ButtonGroup, Container, Col, Row, Image} from 'react-bootstrap'
+import {Nav, Navbar, Form, FormControl,Button,ButtonGroup, Container, Col, Row, Image,ButtonToolbar} from 'react-bootstrap'
 // import {Link} from 'react-router-dom' problem here !
 import aoc from './images/aoc.png'
 
@@ -22,10 +22,16 @@ export default function Navbars() {
             <Nav.Link style={{fontWeight: 'bold', fontSize: '1.2rem'}} href="#link">Concat</Nav.Link>
             <Nav.Link style={{fontWeight: 'bold', fontSize: '1.2rem'}} href="/gallery">gallery</Nav.Link>
         </Nav>     
-        <ButtonGroup>
-                <Button  style={{ fontWeight: 'bold'}} variant="light" size="lg" >Login</Button>
-                <Button  style={{ fontWeight: 'bold'}} variant="light" size="lg">Register</Button>
-            </ButtonGroup>
+        <ButtonToolbar>
+                <Button  className="mr-3" style={{ fontWeight: 'bold', color:'#777'}} variant="light" size="lg" >Login</Button>
+                <Button  style={{ fontWeight: 'bold'}} variant="outline-success" size="lg">Register</Button>
+            </ButtonToolbar>
+            {/* <ButtonToolbar>
+            <Button  style={{ fontWeight: 'bold'}} variant="light" size="lg" >Login</Button>
+            <Button style={{ fontWeight: 'bold'}} variant="outline-success">Register</Button>
+
+
+            </ButtonToolbar> */}
        </Navbar.Collapse>
       </Container>
     </Navbar>
