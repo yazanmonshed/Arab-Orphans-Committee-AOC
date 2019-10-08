0b621c20342d7782cbf187ce9f81809e6dc89add
Yazan Al Monshed <yazanalmonshed@localhost.localdomain>
2019-10-08 22:32:54 +0300

    edit files project

--- frontend_site/gui/src/App.js
+++ frontend_site/gui/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import './App.css';
+
+// import pages here 
+import About from './pages/About';
+import Homepage from './pages/Homepage' 
+import Gallery from './pages/Gallery'
+class App extends Component {
+  render() {
+    return (
+      <div> 
+          <Route exact path="/homepage" component={Homepage} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/gallery" component={Gallery} />
+      </div>
+    )
+  }
+} 
+
 
 export default App;
