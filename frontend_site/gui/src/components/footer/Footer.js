7b8d91daeb22159b9ef6aa52eb1d5c6b8dd55a39
Yazan Al Monshed <yazanalmonshed@localhost.localdomain>
2019-10-24 20:14:23 +0300

    add footer to login page

--- frontend_site/gui/src/components/footer/Footer.js
+++ frontend_site/gui/src/components/footer/Footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import './footer.scss'
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="footer">
+                <hr />
+                <h6>Copyright &copy; 2020 Arab Orphans Committe</h6>
             </div>
         )
     }
