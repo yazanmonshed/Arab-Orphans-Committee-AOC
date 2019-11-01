2601a3f0151eef44a989fc37e3a96f5e28aa9bc0
Yazan Al Monshed <yazanalmonshed@localhost.localdomain>
2019-11-01 19:20:53 +0200

    edd chart js and statstic chart

---
+++ frontend_site/gui/src/pages/home/components_home/Statstic.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {Bar, Line,Pie} from 'react-chartjs-2';
+
+export default class ChartMajor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ChartData : {   
+                labels: ['العلوم', 'الحاسوب', 'العلوم الانسانية والاجتماعي'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [57, 29, 14],
+                    backgroundColor: [
+                        '#78e08f',
+                        '#e55039',
+                        '#4a69bd',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            
+
+            }
+        }
+    }
+  render() {
+    return (
+      <section> 
+          <Pie 
+          data={this.state.ChartData}
+            // options={{
+            //     title: {
+            //         display: true,
+            //         text: 'توزيع الطلاب حسب التخصصات',
+            //         fontSize: 30,
+
+            //     }
+            // }}
+          />
+      </section>
+    );
+  }
+}
