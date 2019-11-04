eb0c81cbe61c913c2967fc1bd4cdb64401ad387d
Yazan Al Monshed <yazanalmonshed@localhost.localdomain>
2019-11-04 10:19:46 +0200

    add some edit

--- backend_site/src/djreact/urls.py
+++ backend_site/src/djreact/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+router = routers.DefaultRouter()
+from blog.views import Data_blog_api
+router.register('', Data_blog_api)
 
 urlpatterns = [
+    path('blog_api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
