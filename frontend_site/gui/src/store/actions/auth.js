e968062e42f6eb327512f7009be0ba28e14945b8
Yazan Al Monshed <yazanalmonshed@localhost.localdomain>
2019-11-17 12:59:45 +0200

    add  redux store

---
+++ frontend_site/gui/src/store/actions/auth.js
@@ -0,0 +1,118 @@
+import * as actionType from './actionType'
+import axios from 'axios'
+
+export const authStart = () => {
+    return {
+        type: actionType.AUTH_START
+    }
+}
+
+
+export const authSuccess = (token) => {
+    return {
+        type : actionType.AUTH_SUCCESS,
+        token : token
+    }
+}  
+
+
+export const authFail = (error) => {
+    
+    return {
+        type: actionType.AUTH_FAIL,
+        error: error
+    }
+}
+
+const logout = (token) => {
+    localStorage.removeItem('user')
+    localStorage.removeItem('expirationDate')
+
+    return {
+        type:  actionType.AUTH_LOGOUT,
+        token: token
+    }
+
+}
+
+const checkAuthtimeout  = expirationTime => {
+    return dispatch => {
+       setTimeout(() => {
+
+            dispatch(logout())
+       }, expirationTime * 1000)
+    }
+}
+
+export const authLogin = (username, password) => {
+
+    return dispatch => {
+        dispatch(authStart())
+        axios.post('127.0.0.1:8000', {
+            username: username,
+            password: password
+        })
+        .then(res => {
+            const token = res.data.key;
+            const expirationDate = new Date(new Date().getTime() + 3600 * 1000)
+            localStorage.setItem('token', token)
+            localStorage.setItem('expirationDate', expirationDate)
+            dispatch(authSuccess(token))
+            dispatch(checkAuthtimeout(3600))
+
+        })
+        .catch(err => {
+            dispatch(authFail(err))
+        })
+    }
+}
+
+
+// Singup Page Here 
+export const authSignup = (username,email, password1, password2) => {
+
+    return dispatch => {
+        dispatch(authStart())
+        axios.post('127.0.0.1:8000', {
+            username: username,
+            email: email,
+            password1: password1,
+            password2: password2
+        })
+        .then(res => {
+            const token = res.data.key;
+            const expirationDate = new Date(new Date().getTime() + 3600 * 1000)
+            localStorage.setItem('token', token)
+            localStorage.setItem('expirationDate', expirationDate)
+            dispatch(authSuccess(token))
+            dispatch(checkAuthtimeout(3600))
+ 
+        })
+        .catch(err => {
+            dispatch(authFail(err))
+        })
+    }
+}
+
+
+
+
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token  = localStorage.getItem('token')
+
+        if (token === undefined) {
+
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'))
+            if (expirationDate <= new Date())   {
+                dispatch(logout())
+            } else {
+                dispatch(authSuccess(token))
+                dispatch(checkAuthtimeout((expirationDate.getTime() - new Date().getTime()) / 1000 ))
+            }
+          }
+    }
+}  
---
+++ frontend_site/gui/src/store/utility.js
@@ -0,0 +1,9 @@
+export const updateObject  = (oldObject, updateProperties) => {
+     
+    return {
+        ...oldObject,
+        ...updateProperties
+    }
+} 
+ 
+ 
