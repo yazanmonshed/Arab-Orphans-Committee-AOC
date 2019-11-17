c02b9e56845713db549a783ef05df093e5dc1ba4
Yazan Al Monshed <yazanalmonshed@localhost.localdomain>
2019-11-17 12:56:21 +0200

    add some date ad auth to user page

--- backend_site/src/blog/models.py
+++ backend_site/src/blog/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class Blog(models.Model):
-    image = models.ImageField(upload_to="post/")
+    image = models.ImageField(upload_to="images/")
     title =  models.CharField(max_length=100)
     # context = models.TextField()
-    date = models.DateField()
+    date = models.DateTimeField()
 
     def __str__(self):
         return self.title
