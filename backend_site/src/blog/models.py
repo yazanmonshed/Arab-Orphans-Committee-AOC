aba1916ab36ff8ed5e7ceb1c121a5dc83cd9d9f0
Yazan Al Monshed <yazanalmonshed@localhost.localdomain>
2019-11-19 14:01:47 +0200

    edit models blog  app

--- backend_site/src/blog/models.py
+++ backend_site/src/blog/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 
 class Blog(models.Model):
     image = models.ImageField(upload_to="images/")
     title =  models.CharField(max_length=100)
-    # context = models.TextField()
-    date = models.DateTimeField()
-
+    context = models.TextField(max_length=1000,default='foo')
+    date = models.DateTimeField()   
     def __str__(self):
         return self.title
 
