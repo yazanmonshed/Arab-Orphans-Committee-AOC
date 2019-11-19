f8efd22d62046f516b43af1bec96ae26f94a246b
Yazan Al Monshed <yazanalmonshed@localhost.localdomain>
2019-11-19 14:04:27 +0200

    add link to blog

--- frontend_site/gui/src/pages/home/components_home/articles.js
+++ frontend_site/gui/src/pages/home/components_home/articles.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Col, Card,CardDeck} from 'react-bootstrap'
-
-
+import {Link} from 'react-router-dom'
+import '../Homepage.scss'
 class  Articles  extends  Component  {
 
     render() {
@@ -9,20 +9,21 @@ class  Articles  extends  Component  {
         return (
             <React.Fragment>
             <Col xs={12} sm={6}  lg={4}> 
-                <CardDeck style={{width: '100%', height: '250px',marginTop: '20px', boxShadow: '1px 2px 9px'}}>
-                <Card>
-                <Card.Img variant="top" src={this.props.info.image} />
-                <Card.Body> 
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                {context}
-                </Card.Text>    
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted">{date}</small>
-                </Card.Footer>
-            </Card>
-        </CardDeck>
+            <Link to='/blog' style={{textDecoration: 'none'}}>
+                <CardDeck>
+                <Card className="custom-articles">
+                    <Card.Img variant="top" src={image} />
+                    <Card.Body> 
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                    </Card.Text>    
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">{date}</small>
+                    </Card.Footer>
+                </Card>
+            </CardDeck>
+        </Link>
         </Col>
 </React.Fragment>
         );
