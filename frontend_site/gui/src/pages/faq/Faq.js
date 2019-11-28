131631701f8746f04740cbfaf7b3802a6208cb0f
Yazan Al Monshed <yazanalmonshed@localhost.localdomain>
2019-11-28 14:38:24 +0200

    almost finsied the FAQ page without style

--- frontend_site/gui/src/pages/faq/Faq.js
+++ frontend_site/gui/src/pages/faq/Faq.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
 import Navbar_faq from '../../components/navbar_faq/navbar_faq'
-import {Breadcrumb, Container} from 'react-bootstrap'
+import {Breadcrumb, Container, Row,Col,Jumbotron,Button} from 'react-bootstrap'
+import Questions from './component_faq/QuestionsFaq'
+import FooterMainPage from '../../components/footer/FooterMainPage'
+
+import axios from 'axios'
+
+
 export default class Faq extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+
+        }
+    }
+
+     componentDidMount() {
+
+        axios.get('http://127.0.0.1:8000/faq_api/')
+        .then(res =>  {
+
+            this.setState({data: res.data})
+        })
+
+     }
+
     render() {
         return (
             <div>
@@ -17,6 +43,23 @@ export default class Faq extends Component {
                         </Breadcrumb.Item>
                         </Container>
                     </Breadcrumb>
+                    <Row>
+                    <Col> 
+                <Jumbotron style={{background:'#FFF'}}>
+                    <Container>
+                        <h1>الأسئلة الأكثر تكرارا</h1>
+                        <p >
+                        تحتوي هذه الصفحة على الاسئلة الشائعة من قبل الاهالي والطلبة
+                        </p>
+                        {/* <p>
+                            <Button variant="primary">Learn more</Button>
+                        </p> */}
+                        </Container>
+                </Jumbotron>                      
+                 </Col>
+                </Row>
+                    {this.state.data.map(res => <Questions questions={res.questions} answers={res.answers} />)}
+                    <FooterMainPage/>
             </div>
         )
     }
---
+++ frontend_site/gui/src/pages/faq/component_faq/QuestionsFaq.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {Container,Accordion,Card,Button} from 'react-bootstrap'
+
+export default class Questions extends Component {
+  render() {
+      const {questions, answers} = this.props
+    return (
+      <div>  
+           <Container>
+                    <Accordion defaultActiveKey="1">
+                        <Card>
+                            <Card.Header>
+                           
+                                <Accordion.Toggle as={Button} variant="link" eventKey="0" activeKey="1">
+                                    {questions}
+                                    <i className="fas fa-plus" style={{margin: '10px'}}></i>
+                                </Accordion.Toggle>
+                            </Card.Header>
+                                <Accordion.Collapse eventKey="0">
+                                <Card.Body> {answers} </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+               </Accordion>
+         </Container>
+      </div>
+    );
+  }
+}
